de7b67d076df25cce2a543011f7b6702c31d80ab
Mir Sadat Bin Rakib <sadatmir13@gmail.com>
2025-05-05 10:02:15 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/tab-whisperer/', 
   plugins: [react()],
-});
+})
